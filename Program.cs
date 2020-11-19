fbc323087ba32dea76efdd2834c2edb8e102cfad
giovanni iannace <giovanniiannace56@gmail.com>
2020-11-19 18:07:07 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace array_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá , como vc está!");string[] listaCompras = {"maçã", "batata", "cenoura"};
+            double[] precos = {1.1, 2.3, 4.5};
+            int [] idades = {16, 17, 17};
+
+
+
+            Console.WriteLine("Qual o tamanho da lista que você quer?");
+            int tamanho = int.Parse(Console.ReadLine());
+
+            string[] nomesAlunos = new string[tamanho];
+
+            for (int i = 0; i < nomesAlunos.Length; i++)
+            {
+                Console.WriteLine("Digite um nome: ");
+                nomesAlunos[i] = Console.ReadLine();
+            }
+
+            for (var i = 0; i < nomesAlunos.Length; i++)
+            {
+                Console.WriteLine(nomesAlunos[1]);
+            }
+        }
+    }
+}
